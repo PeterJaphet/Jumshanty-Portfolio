0802d9150852bd9ee5879f8458d01061c1b35d66
Peter Japhet <peterjaphet111@gmail.com>
2023-06-09 04:03:34 +0300

    Worked on the portfolio videos

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-/*  ---------------------------------------------------
-    Template Name: Dreams
-    Description: Dreams wedding template
-    Author: Colorib
-    Author URI: https://colorlib.com/
-    Version: 1.0
-    Created: Colorib
----------------------------------------------------------  */
 
 'use strict';
 
